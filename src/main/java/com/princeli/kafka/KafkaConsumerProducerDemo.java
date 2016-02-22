53e2f9e3b92f4474aafeba31a9a109472f8c3fdd
princeli <princeli@live.cn>
2016-02-22 09:41:57 +0800

    first commit

---
+++ src/main/java/com/princeli/kafka/KafkaConsumerProducerDemo.java
@@ -0,0 +1,12 @@
+package com.princeli.kafka;
+
+public class KafkaConsumerProducerDemo {
+	public static void main(String[] args)  
+    {  
+        KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);  
+        producerThread.start();  
+  
+        KafkaConsumer consumerThread = new KafkaConsumer(KafkaProperties.topic);  
+        consumerThread.start();  
+    }  
+}
